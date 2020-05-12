150ba53a7fab8783792bd637ef0042f2541c5065
Fajar Robert K H <fajarrobertkh@gmail.com>
2020-05-12 08:12:31 +0700

    add webview monitor

--- app/src/main/java/com/srsssms/ActivityMonitor.kt
+++ app/src/main/java/com/srsssms/ActivityMonitor.kt
@@ -2,11 +2,24 @@ package com.srsssms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_monitor.*
 
 class ActivityMonitor : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monitor)
+        val myWebView: WebView = findViewById(R.id.webView)
+        myWebView.settings.javaScriptEnabled = true
+        myWebView.getSettings().setBuiltInZoomControls(false)
+        myWebView.webViewClient = WebViewClient()
+        myWebView.loadUrl("https://covid.srs-ssms.com/admin/login")
+    }
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        }
     }
 }
